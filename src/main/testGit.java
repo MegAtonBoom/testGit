de10e61679bd483bc66c87ca3405079c4b6c6ff2
MegAtonBoom <baba.adrian13@gmail.com>
2022-04-15 11:28:32 +0200

    added comment 1

--- src/main/testGit.java
+++ src/main/testGit.java
@@ -1,12 +1,10 @@
 package main;
 public class testGit {
     public static void main(String[] args) {
-        //String a=System.getProperty("java.class.path");
-        //System.out.println("Hello World!" + a); // Display the string.
         stampa();
     }
     public static void stampa(){
-
+        //print
         System.out.println("stampa23");
     }
 }
