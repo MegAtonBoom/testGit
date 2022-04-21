6c9e89c72097d504f976721e28cd9284d8da8a6c
MegAtonBoom <baba.adrian13@gmail.com>
2022-04-21 11:08:07 +0200

    added comment prints

--- src/main/testGit.java
+++ src/main/testGit.java
@@ -6,7 +6,7 @@ public class testGit {
     public static void stampa(){
         int a=20;
         //print
-
+        //3 prints
         System.out.println(a+" test\n");
         System.out.println(a+" test2");
         System.out.println(a+" test3");
